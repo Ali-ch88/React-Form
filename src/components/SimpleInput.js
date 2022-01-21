0297c4ffab8d021d986661223501e9943e780409
Ali-ch88 <raza7861991@gmail.com>
2022-01-21 12:31:26 +0500

    access data

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [initialValue, setInitialValue] = useState("");
+
+  const inputHandler = (event) => {
+    setInitialValue(event.target.value);
+  };
+
+  const formHandler = (event) => {
+    event.preventDefault();
+    console.log(initialValue);
+    setInitialValue("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          value={initialValue}
+          type="text"
+          id="name"
+          onChange={inputHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
